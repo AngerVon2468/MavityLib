ec8d2eff4564d1094e91ec9e6badc9a4965d7a5b
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-10 14:46:10 +1000

    Just in case

--- src/main/java/wiiu/mavity/mavity_lib/property/PropertyInit.java
+++ src/main/java/wiiu/mavity/mavity_lib/property/PropertyInit.java
@@ -5,8 +5,8 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import wiiu.mavity.mavity_lib.MavityLib;
 
 public class PropertyInit {
-    public static final FabricItemSettings GT = new FabricItemSettings().maxCount(64);
-    public static final FabricItemSettings GTOI = new FabricItemSettings().maxDamage(0);
+    public static FabricItemSettings GT = new FabricItemSettings().maxCount(64);
+    public static FabricItemSettings GTOI = new FabricItemSettings().maxDamage(0);
     public static void registerPropertyInit() {
         MavityLib.LOGGER.info(MavityLib.MOD_ID + " has registered its' properties.");
     }
