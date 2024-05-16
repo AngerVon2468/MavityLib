40524b727318f3c64374d354e3dca3f67d70083a
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-16 18:40:28 +1000

    1.2.2 & upload to modrinth and curseforge

--- src/main/java/wiiu/mavity/mavity_lib/mixin/ExampleMixin.java
+++ src/main/java/wiiu/mavity/mavity_lib/mixin/ExampleMixin.java
@@ -6,10 +6,13 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import wiiu.mavity.mavity_lib.MavityLib;
+
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
 		// This code is injected into the start of MinecraftServer.loadWorld()V
+		MavityLib.LOGGER.info("LOGGER SPAM!!! :D");
 	}
 }
