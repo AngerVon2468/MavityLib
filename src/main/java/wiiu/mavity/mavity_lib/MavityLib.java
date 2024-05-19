476c8a1478ab607c39bd885eb5f430b7bc7ed4a6
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-19 11:10:42 +1000

    1.3.3

--- src/main/java/wiiu/mavity/mavity_lib/MavityLib.java
+++ src/main/java/wiiu/mavity/mavity_lib/MavityLib.java
@@ -30,6 +30,8 @@ public class MavityLib implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
+	public static final String NOTE = "Main Menu Credits translations are removed from Mavity Lib so mods that use it can override them without lang file issues.";
+
 	@Override
 	public void onInitialize() {
 
