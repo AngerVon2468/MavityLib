d23699624da5a13d3b933f05ce84ea4ffefe68bb
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-22 14:09:17 +1000

    4.2.0

--- src/main/java/wiiu/mavity/mavity_lib/MavityLibClient.java
+++ src/main/java/wiiu/mavity/mavity_lib/MavityLibClient.java
@@ -2,6 +2,7 @@ package wiiu.mavity.mavity_lib;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+
 import wiiu.mavity.mavity_lib.event.EventCallbacks;
 import wiiu.mavity.mavity_lib.keybind.KeyBindInit;
 
