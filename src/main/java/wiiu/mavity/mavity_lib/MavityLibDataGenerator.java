b62cf6a37bdcd796020565ff3a592cb751ce2cdd
AngerVon2468 <thomaschad0909@gmail.com>
2024-05-24 20:18:20 +1000

    5.0.0

--- src/main/java/wiiu/mavity/mavity_lib/MavityLibDataGenerator.java
+++ src/main/java/wiiu/mavity/mavity_lib/MavityLibDataGenerator.java
@@ -2,9 +2,12 @@ package wiiu.mavity.mavity_lib;
 
 import net.fabricmc.fabric.api.datagen.v1.*;
 
+import wiiu.mavity.mavity_lib.util.exception.AntiDataGenException;
+
 public class MavityLibDataGenerator implements DataGeneratorEntrypoint {
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		// Ew datagen
+		throw new AntiDataGenException("Ew datagen!!");
 	}
 }
---
+++ src/main/java/wiiu/mavity/mavity_lib/util/exception/AntiDataGenException.java
@@ -0,0 +1,11 @@
+package wiiu.mavity.mavity_lib.util.exception;
+
+/**
+ * Datagen should fear this class.
+ */
+public class AntiDataGenException extends RuntimeException {
+
+    public AntiDataGenException(String s) {
+        super(s);
+    }
+}
